92fa749776b0b202d7aa5af578e1e35bb141f605
Jim Lee <gitHub@leftcoast.biz>
2023-03-11 14:02:27 -0800

    Update slowServo.cpp

--- src/slowServo.cpp
+++ src/slowServo.cpp
@@ -40,6 +40,7 @@ void slowServo::setDeg(int inDeg) { desiredDeg = inDeg; }
 // called if begin is not called to set things up beforehand.
 void slowServo::idle(void) {
 
+
   if (currentDeg!=desiredDeg && degTimer.ding()) {
     if (currentDeg>desiredDeg) {
       currentDeg--;
